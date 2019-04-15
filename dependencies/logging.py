618839a4f6f852356b49e772bc3c260f74f78c92
mehd-io <mehdi@mehd.io>
2019-04-15 16:20:10 +0200

    add yarn logger object

--- dependencies/logging.py
+++ dependencies/logging.py
@@ -6,6 +6,9 @@ This module contains a class that wraps the log4j object instantiated
 by the active SparkContext, enabling Log4j logging for PySpark using.
 """
 
+import os
+import logging
+import sys
 
 class Log4j(object):
     """Wrapper class for Log4j JVM object.
@@ -15,9 +18,8 @@ class Log4j(object):
 
     def __init__(self, spark):
         # get spark app details with which to prefix all messages
-        conf = spark.sparkContext.getConf()
-        app_id = conf.get('spark.app.id')
-        app_name = conf.get('spark.app.name')
+        app_id = spark.sparkContext.getConf().get('spark.app.id')
+        app_name = spark.sparkContext.getConf().get('spark.app.name')
 
         log4j = spark._jvm.org.apache.log4j
         message_prefix = '<' + app_name + ' ' + app_id + '>'
@@ -49,3 +51,18 @@ class Log4j(object):
         """
         self.logger.info(message)
         return None
+
+class YarnLogger:
+    @staticmethod
+    def setup_logger():
+        if not 'LOG_DIRS' in os.environ:
+            sys.stderr.write('Missing LOG_DIRS environment variable, pyspark logging disabled')
+            return 
+
+        file = os.environ['LOG_DIRS'].split(',')[0] + '/pyspark.log'
+        logging.basicConfig(filename=file, level=logging.INFO, 
+                format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s')
+
+    def __getattr__(self, key):
+        return getattr(logging, key)
+
